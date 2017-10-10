432d50bb9c1bc106fe5e1e067b5026b760b338c5
YYF <iyuanyunfeng@163.com>
2017-10-10 19:35:41 +0800

    删除HelloWorld

--- src/main/java/com/dt/demo/HeguihaoCotroller.java
+++
@@ -1,4 +0,0 @@
-package com.dt.demo;
-
-public class HeguihaoCotroller {
-}
--- src/main/java/com/dt/demo/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.dt.demo;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("HelloWorld!");
-    }
-}
